a62c3788061af8e9c8ccc67cb44f16c6645f6749
Jean Paul Ordoñez Ibarguen <jeandeveloper04@gmail.com>
2025-05-21 22:25:19 -0500

    Update util.js

--- Cliente/assets/utils/index/util.js
+++ Cliente/assets/utils/index/util.js
@@ -1,6 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    console.log('DOM Loaded :)');
+    // Obtener los campos del formulario
+
+    const correo = document.getElementById("email");
+    const contrasena = document.getElementById("password");
+
+    // Obtener los botones del formulario
+
+    const btnLogin = document.getElementsByClassName("btn btn-primary")[0];
+
+    // Event listener para el botón de registro
+
+    btnLogin.addEventListener("click", function(e){
+        e.preventDefault();
+
+        const url = "proyectolaravel.railway.internal/api/login/user";
+
+        const data = {
+            correo: correo.value,
+            contrasena: contrasena.value
+        }
+
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }).then(response => response.json())
+        .then(data => {
+            if(data.status === 200){
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Login exitoso',
+                    text: 'Usuario logueado correctamente',
+                    showConfirmButton: false,
+                    timer: 1500
+                }).then(() => {
+                    localStorage.setItem("token", data.token);
+                    sessionStorage.setItem("user", JSON.stringify(data.user));
+                    sessionStorage.setItem("id", data.user_id);
+                    window.location.href = "./views/dashboard/dashboard.html";
+                    
+                });
+            }else{
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: data.message,
+                    showConfirmButton: true,
+                    confirmButtonText: 'Aceptar'
+                });
+            }
+        }).catch(error => {
+            console.error("Error:", error);
+        });
+
+
+
+    })
 
 
 
